771afa98c39ce7ee0789cc126fa782fe4ea5b4ef
Evelynpa <evelyn.parra.rojas@gmail.com>
2017-11-07 10:13:11 -0300

    Se realiza Versión 0.0.2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,48 @@
+
 function add(){
 
-
-	//obtener el valor del texarea
+			//obtener el valor del texarea
 	var text = document.getElementById('textarea').value;
-	var nodoText = document.createTextNode(text);
+		//crear etiqueta p
+		var campP = document.createElement('p');
+		campP.classList.add('textResult');
+		var nodoText = document.createTextNode(text);
 
-	//crear la nueva seccion
-	var campSpan = document.createElement('p');
+		//agregando un id a la nueva seccion
+		campP.appendChild(nodoText);
 
-	//agregando un id a la nueva seccion
-	campSpan.appendChild(nodoText);
 
-	//crear hora
-	var spanHour = document.createElement('span');
-	spanHour.setAttribute('id', 'hora');
+		//crear nueva seccion
+		var sectionBottom = document.createElement('section');
+		sectionBottom.setAttribute('id', 'containerBottom')
+		sectionBottom.appendChild(campP);
+		//sectionBottom.appendChild(camSpan);
 
-	//funcion hora
+		document.body.appendChild(sectionBottom);
+
+	
+	/*limpiar textarea*/
+	document.getElementById("textarea").value = "";
+}
+
+var btnSubmit = document.getElementById('btnButton');
+btnSubmit.addEventListener('click', add);
+
+//funcion desabilitar boton
+
+function disableButton(){
+
+	var text = document.getElementById('textarea').value;
+	var btnTweet = document.getElementById('btnButton');
+	if (text.length === '') {
+		btnTweet.disabled = false;
+	}else {
+		btnTweet.disabled = false;
+	}
+}
+
+
+//funcion hora
 	function actualizaHora(){
 	var fecha = new Date();
 
@@ -23,25 +50,32 @@ function add(){
 	var minutos = fecha.getMinutes();
 	var segundos = fecha.getSeconds();
 
-	//var spanHour = document.getElementById('hora');
+	var spanHour = document.getElementById('hora');
 
 	spanHour.textContent = hora +':'+ minutos +':'+ segundos;
 	}
 	setInterval(actualizaHora,1000);
 
+//funcion contar
+function contar() { 
+         var max = "140"; 
+         var cadena = document.getElementById('textarea').value;
+         var longitud = cadena.length; 
+         var cont = document.getElementById("contador").value = max-longitud;
+         
 
-	var nodoHour = document.createTextNode(text);
+             if(longitud <= max) { 
+             	//document.getElementById("contador").value = max-longitud;
+             	if(cont >=130){ 
+      				document.getElementById("contador").style.color = '#0000ff';
+             	}
+             	else if(cont >=120){
+             		document.getElementById("contador").style.color = '#00ff00';
+             	}
 
-
-	var sectionBottom = document.createElement('section');
-	sectionBottom.setAttribute('id', 'containerBottom')
-	sectionBottom.appendChild(campSpan);
-	sectionBottom.appendChild(spanHour);
-
-	document.body.appendChild(sectionBottom);
-}
-
-var btnSubmit = document.getElementById('btnButton');
-btnSubmit.addEventListener('click', add);
-
-
+             } else { 
+                  document.getElementById('textarea').value = cadena.substr(0, max);
+                  document.getElementById('btnButton').disabled = true;
+                  
+             } 
+    } 
